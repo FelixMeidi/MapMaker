e26c3ea426d529412a13559d37325e7c8475a33a
felix <felixmeidi123@gmail.com>
2025-06-26 14:12:32 +0200

    new working update

--- core/src/de/meidi/game/worldbuilding/UserInput.java
+++ core/src/de/meidi/game/worldbuilding/UserInput.java
@@ -48,6 +48,7 @@ public class UserInput implements InputProcessor
         return false;
     }
 
+
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button)
     {
